fa2945729d09fbc917b7b0a3a7b47c7062e307b4
5819786 <162524763+abwlzjf@users.noreply.github.com>
2024-03-14 20:25:02 +0900

    Update 0312_5819786.c
    korean

--- Project1/0312_5819786.c
+++ Project1/0312_5819786.c
@@ -18,6 +18,6 @@ main(void) {
             sum += i;
         }
     }
-    printf("0ºÎÅÍ %d±îÁö¿¡¼­ ¼Ò¼öÀÇ ÇÕ: %d", MAX, sum);
+    printf("0ë¶€í„° 100ê¹Œì§€ì˜ ì†Œìˆ˜ì˜ í•©: %d", MAX, sum);
 	return 0;
 }
