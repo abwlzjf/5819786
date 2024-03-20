80baa9b5963ca0779f6a90355a8f16046971a42c
samsung-user <samsung-user@DESKTOP-LH9FVAI>
2024-03-21 01:44:19 +0900

    code

--- Project1/5819786_0319_01.c
+++ Project1/5819786_0319_01.c
@@ -2,34 +2,38 @@
 #include <time.h>
 
 double factorial_iter(int n) {
-    double result = 1;
-    for (int i = 1; i <= n; i++) {
-        result *= i;
-    }
-    return result;
+	double result = 1;
+	for (int i = 1;i <= n;i++) {
+		result *= i;
+	}
+	return result;
 }
 
 double factorial_rec(int n) {
-    if (n <= 1)
-        return 1;
-    else
-        return n * factorial_rec(n - 1);
+	if (n <= 1) return 1;
+	else
+		return n * factorial_rec(n - 1);
 }
 
 int main() {
-    clock_t start, end;
+	clock_t start, end;
+	double time;
+	double result_rec, result_iter;
 
-    start = clock();
-    double result_iter = factorial_iter(20);
-    end = clock();
-    printf("Factorial Iterative Result: %lf\n", result_iter);
-    printf("Time: %f\n", ((float)(end - start)) / CLOCKS_PER_SEC);
+	start = clock();
+	result_iter = factorial_iter(20);
+	end = clock();
 
-    start = clock();
-    double result_rec = factorial_rec(20);
-    end = clock();
-    printf("Factorial Recursive Result: %lf\n", result_rec);
-    printf("Time: %f\n", ((float)(end - start)) / CLOCKS_PER_SEC);
+	time = ((double)(end - start)) / CLOCKS_PER_SEC);
+	printf("Factorial Recursive Result: %lf\n", result_iter);
+	printf("Time: %lf\n", time);
 
-    return 0;
+	start = clock();
+	result_rec = factorial_rec(20);
+	end = clock();
+
+	time = ((double)(end - start)) / CLOCKS_PER_SEC);
+	printf("Factorial Recursive Result: %lf\n", result_rec);
+	printf("Time: %lf\n", time);
+	return 0;
 }
